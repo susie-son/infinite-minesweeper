f0dcb7db99a29dcdfb7c7888ec34503d3c84883c
Susie Son <susieson99@gmail.com>
2024-03-12 11:23:43 -0700

    remove margin

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { createGlobalStyle } from 'styled-components'
 import Game from './Game'
 
+const GlobalStyle = createGlobalStyle`
+  html, body {
+    padding: 0;
+    margin: 0;
+    overflow: hidden;
+  }
+`
+
 const App = () => {
-  return <Game />
+  return (
+    <>
+      <GlobalStyle />
+      <Game />
+    </>
+  )
 }
 
 export default App
